c73d977cd6cb4bfb5baf87ae0cdece3a0a60d673
steve <stevemighty2030@gmail.com>
2022-08-18 09:31:28 +0300

    the assignment is complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'John';
+// variable customerName is part of the scope chain of upperCaseCustomerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// variable has been declared globally
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(bestCustomerName) {
+  return bestCustomer = bestCustomerName;
+}
+
+function changeLeastFavoriteCustomer () {
+    return leastFavoriteCustomer = 'steve';
+}
